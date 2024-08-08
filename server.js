0941dca4f9be0b3274c27e242bb32bf10b1d4fdc
lucilefonlupt <fonlupt.lucile@gmail.com>
2024-08-08 08:57:04 +0200

    rute

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5147;
 
 // Configure CORS
 app.use(cors({
-  origin: 'https://yggdrasil.wouafwouaf.ovh'
+  origin: '*' // Utilisez * temporairement pour tester, remplacez par l'origine correcte plus tard
 }));
 
 // Servir les fichiers statiques du dossier 'build'
@@ -22,8 +22,21 @@ const getMondes = () => {
 
 // Routes API
 app.get('/api/mondes', (req, res) => {
+  console.log("Received request for /api/mondes");
   const mondes = getMondes();
   res.json(mondes);
+});
+
+// Route pour un monde par ID
+app.get('/api/mondes/:id', (req, res) => {
+  console.log(`Received request for /api/mondes/${req.params.id}`);
+  const mondes = getMondes().mondes;
+  const monde = mondes.find(m => m.id === req.params.id);
+  if (monde) {
+    res.json(monde);
+  } else {
+    res.status(404).send('Monde not found');
+  }
 });
 
 // Pour toutes les autres requêtes, retourner le fichier 'index.html' du dossier 'build'
