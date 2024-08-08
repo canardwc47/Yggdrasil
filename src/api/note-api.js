29456c49fb9a390fb6efe8323594f4902a965fa6
lucilefonlupt <fonlupt.lucile@gmail.com>
2024-08-08 11:16:44 +0200

    api

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -26,33 +26,5 @@ export class WorldAPI {
       });
   }
 
-  static async fetchWorldById(id) {
-    return fetch(`${BASE_URL}/${id}`)
-      .then(response => {
-        console.log("Response:", response);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.text().then(text => {
-          console.log("Response Text:", text);
-          try {
-            return JSON.parse(text);
-          } catch (error) {
-            console.error("Error parsing JSON:", error, "Response text:", text);
-            throw new Error("Invalid JSON");
-          }
-        });
-      })
-      .then((data) => {
-        return data;
-      })
-      .catch((error) => {
-        console.error(`There was a problem with the fetchWorldById operation for ID ${id}:`, error);
-      });
-  }
 }
-
-
-
-
 
