9754790ede652caf343677638570cdc422c084f9
lucilefonlupt <fonlupt.lucile@gmail.com>
2024-08-08 11:24:06 +0200

    api

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -4,27 +4,15 @@ export class WorldAPI {
   static async fetchAllWorlds() {
     return fetch(BASE_URL)
       .then(response => {
-        console.log("Response:", response);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        return response.text().then(text => {
-          console.log("Response Text:", text);
-          try {
-            return JSON.parse(text);
-          } catch (error) {
-            console.error("Error parsing JSON:", error, "Response text:", text);
-            throw new Error("Invalid JSON");
-          }
-        });
-      })
-      .then((data) => {
-        return data;
+        return response.json();
       })
       .catch((error) => {
         console.error("There was a problem with the fetchAllWorlds operation:", error);
       });
   }
-
 }
+
 
